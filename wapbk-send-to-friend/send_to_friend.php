67c74ae43720c86b1bab042a0d5c0dd082808995
pinalj <pinalj1612@gmail.com>
2015-10-12 17:04:50 +0530

    Fix #22
    This fix ensures that booking details are not pre-populated for
    cancelled/refunded/trashed orders.

--- wapbk-send-to-friend/send_to_friend.php
+++ wapbk-send-to-friend/send_to_friend.php
@@ -404,212 +404,220 @@ if ( !class_exists( 'send_to_friend' ) ) {
 				} else {
 					$attribute_fields_str = ',tyche: 1';
 				}
-				$booking_settings = get_post_meta( $duplicate_of, 'woocommerce_booking_settings', true );
-				// get the booking details from the woocommerce_order_itemmeta table
-				$booking_details = WC_Abstract_Order::get_item_meta( $item_id );
+				$order_id_query = "SELECT order_id FROM `".$wpdb->prefix."woocommerce_order_items`
+									WHERE order_item_id = %d";
+				$results_order_id = $wpdb->get_results( $wpdb->prepare( $order_id_query, $item_id ) );
 				
-				if ( isset( $booking_details['_wapbk_booking_date'][0] ) ) {
-					$booking_date = $booking_details['_wapbk_booking_date'][0];
-					// Date formats
-					$date_formats = bkap_get_book_arrays('date_formats');
-					// get the global settings to find the date & time formats
-					$global_settings = json_decode(get_option('woocommerce_booking_global_settings'));
-					$date_format_set = $date_formats[$global_settings->booking_date_format];
-					$display_date = date ( $date_format_set, strtotime( $booking_date ) );
-				}
-				$checkout_date = $booking_time = '';
-				$hidden_date_checkout = '';
-				// if multiple days is enabled, fetch the checkout date
-				if ( isset( $booking_settings['booking_enable_multiple_day'] ) && $booking_settings['booking_enable_multiple_day'] == 'on' ) {
-					if ( isset( $booking_details['_wapbk_checkout_date'] ) ) {
-						$checkout_date = $booking_details['_wapbk_checkout_date'];
-						$hidden_date_checkout = date( 'j-n-Y', strtotime( $checkout_date ) );
+				$order = new WC_Order( $results_order_id[0]->order_id );
+				// check if the order is refunded, trashed or cancelled
+				if( isset( $order->post_status ) && ( $order->post_status != 'wc-cancelled' ) && ( $order->post_status != 'wc-refunded' ) && ( $order->post_status != 'trash' ) && ( $order->post_status != '' ) && ( $order->post_status != 'wc-failed' ) ) {
+				
+					$booking_settings = get_post_meta( $duplicate_of, 'woocommerce_booking_settings', true );
+					// get the booking details from the woocommerce_order_itemmeta table
+					$booking_details = WC_Abstract_Order::get_item_meta( $item_id );
+					
+					if ( isset( $booking_details['_wapbk_booking_date'][0] ) ) {
+						$booking_date = $booking_details['_wapbk_booking_date'][0];
+						// Date formats
+						$date_formats = bkap_get_book_arrays('date_formats');
+						// get the global settings to find the date & time formats
+						$global_settings = json_decode(get_option('woocommerce_booking_global_settings'));
+						$date_format_set = $date_formats[$global_settings->booking_date_format];
+						$display_date = date ( $date_format_set, strtotime( $booking_date ) );
 					}
-				}
-				// if time settings is enabled, fetch the time slot selected
-				if( $booking_settings['booking_enable_time'] == 'on' ) {
-					if ( isset( $booking_details['_wapbk_time_slot'][0] ) ) {
-						$booking_time = $booking_details['_wapbk_time_slot'][0];
-						// create a time stamp for the booking date and start time
-						$time_explode = explode( '-', $booking_time );
-						$book_date = $booking_date . ' ' . $time_explode[0];
-						$date_timestamp = strtotime( $book_date );
+					$checkout_date = $booking_time = '';
+					$hidden_date_checkout = '';
+					// if multiple days is enabled, fetch the checkout date
+					if ( isset( $booking_settings['booking_enable_multiple_day'] ) && $booking_settings['booking_enable_multiple_day'] == 'on' ) {
+						if ( isset( $booking_details['_wapbk_checkout_date'] ) ) {
+							$checkout_date = $booking_details['_wapbk_checkout_date'];
+							$hidden_date_checkout = date( 'j-n-Y', strtotime( $checkout_date ) );
+						}
 					}
-				}
-				// check if bookings are still available for the given date/s and/or time
-				$availability = $this->get_availability( $duplicate_of, $booking_date, $checkout_date, $booking_time);
-				
-				$pre_populate = 0;
-				if ($availability > 0 ) {
-					$pre_populate = 1;
-				}
-				else if ( $availability === "Unlimited" ) {
-					$pre_populate = 1;
-				}
-				
-				$current_time = current_time( 'timestamp' );
-				$past_date = $past_time = 1; // means its a future date and time
-				if ( isset( $current_time ) && isset( $date_timestamp ) ) {
-					if ( $date_timestamp < $current_time ) {
-						$past_time = 0; // means the booking date and time hv already passed
+					// if time settings is enabled, fetch the time slot selected
+					if( $booking_settings['booking_enable_time'] == 'on' ) {
+						if ( isset( $booking_details['_wapbk_time_slot'][0] ) ) {
+							$booking_time = $booking_details['_wapbk_time_slot'][0];
+							// create a time stamp for the booking date and start time
+							$time_explode = explode( '-', $booking_time );
+							$book_date = $booking_date . ' ' . $time_explode[0];
+							$date_timestamp = strtotime( $book_date );
+						}
 					}
-				}
-				else if ( strtotime( $booking_date ) < $current_time ) {
-					// As the Booking date is a past date
-					$pre_populate = $past_date = 0;
-				}
-				
-				$hidden_date = date ( 'j-n-Y', strtotime( $booking_date ) );
-	
-				if ( $pre_populate == 1 ) {
-					?>
-					<script type="text/javascript">
-					function bkap_init() {
-						jQuery( "#wapbk_hidden_date" ).val( '<?php echo $hidden_date; ?>' );
-						var split = jQuery( "#wapbk_hidden_date" ).val().split( "-" );
-						var bookingDate = new Date( split[2], split[1]-1, split[0] );
-						<?php
-						if ( isset( $booking_settings['enable_inline_calendar'] ) && $booking_settings['enable_inline_calendar'] == 'on' ) { 
+					// check if bookings are still available for the given date/s and/or time
+					$availability = $this->get_availability( $duplicate_of, $booking_date, $checkout_date, $booking_time);
+					
+					$pre_populate = 0;
+					if ($availability > 0 ) {
+						$pre_populate = 1;
+					}
+					else if ( $availability === "Unlimited" ) {
+						$pre_populate = 1;
+					}
+					
+					$current_time = current_time( 'timestamp' );
+					$past_date = $past_time = 1; // means its a future date and time
+					if ( isset( $current_time ) && isset( $date_timestamp ) ) {
+						if ( $date_timestamp < $current_time ) {
+							$past_time = 0; // means the booking date and time hv already passed
+						}
+					}
+					else if ( strtotime( $booking_date ) < $current_time ) {
+						// As the Booking date is a past date
+						$pre_populate = $past_date = 0;
+					}
+					
+					$hidden_date = date ( 'j-n-Y', strtotime( $booking_date ) );
+		
+					if ( $pre_populate == 1 ) {
 						?>
-							var timestamp = Date.parse( bookingDate ); 
-							if ( isNaN( timestamp ) == false ) { 
-								var default_date_selection = new Date( timestamp );
-								jQuery( "#inline_calendar" ).datepicker( "setDate", default_date_selection );
-							}
-						<?php 
-						} else {
-						?>
-							jQuery( "#booking_calender" ).datepicker( "setDate", bookingDate );
-						<?php 
-						}?>
-						if ( jQuery( "#wapbk_bookingEnableTime" ).val() == "on" && jQuery( "#wapbk_booking_times" ).val() == "YES" ) {
-							var sold_individually = jQuery( "#wapbk_sold_individually" ).val();
-							jQuery( "#ajax_img" ).show();
-							jQuery( ".single_add_to_cart_button" ).hide();	
-							var time_slots_arr = jQuery( "#wapbk_booking_times" ).val();
-							var data = {
-								current_date: jQuery( "#wapbk_hidden_date" ).val(),
-								post_id: '<?php echo $duplicate_of; ?>', 
-								action: 'bkap_check_for_time_slot'
-								<?php echo $attribute_fields_str; ?>
-							};
-							jQuery.post( '<?php echo get_admin_url() . 'admin-ajax.php'; ?>', data, function( response ) {
-								jQuery( "#ajax_img" ).hide();
-								jQuery( "#show_time_slot" ).html( response );
-								// for today if the time slot is past, display a message saying the same
-								<?php 
-								if ( isset( $past_time ) && $past_time == 0 ) {
-								?>
-									// Pre-populate the time slots with the order time slots
-									jQuery( "#time_slot" ).val( '' );
-								<?php 
-								} else {?>
-									// Pre-populate the time slots with the order time slots
-									jQuery( "#time_slot" ).val( '<?php echo $booking_time; ?>' );
-									var time_slot_value = jQuery( "#time_slot" ).val();
-									// Availability display for the time slot selected if setting is enabled			
-									if ( typeof time_slot_value != "undefined" && jQuery( "#wapbk_availability_display" ).val() == "yes" ) {
-										var data = {
-											checkin_date: jQuery( "#wapbk_hidden_date" ).val(),
-											timeslot_value: time_slot_value,
-											post_id: '<?php echo $duplicate_of; ?>', 
-											action: 'bkap_get_time_lockout'
-										};
-										jQuery.post( '<?php echo get_admin_url() . 'admin-ajax.php'; ?>', data, function( response ) {
-											jQuery( "#show_stock_status" ).html( response );
-										});
-									}
-										
-									if ( jQuery( "#time_slot" ).val() != "" ) {
-	                                    jQuery( ".payment_type" ).show();
-										if( sold_individually == "yes" ) {
-											jQuery( ".quantity" ).hide();
-											jQuery( ".payment_type" ).hide();
-											jQuery(".partial_message").hide();
-										} else {
-											jQuery( ".quantity" ).show();
-											jQuery( ".payment_type" ).show();
-										}
-									} else if ( jQuery("#time_slot").val() == "" ) {
-										jQuery( ".single_add_to_cart_button" ).hide();
-										jQuery( ".quantity" ).hide();
-	                                    jQuery( ".payment_type" ).hide();
-										jQuery( ".partial_message" ).hide();
-									}
-									// This is called to ensure the variable pricing for time slots is displayed
-									bkap_single_day_price();
-								<?php 
-								}?>
-							});
-						} else {
-							// check if multiple day is enabled, if yes then pre-populate the end date
-							if ( jQuery( "#booking_calender_checkout" ).length ) {
-								jQuery( "#wapbk_hidden_date_checkout" ).val( '<?php echo $hidden_date_checkout; ?>' );
-								var split = jQuery( "#wapbk_hidden_date_checkout" ).val().split( "-" );
-								var bookingDate = new Date( split[2], split[1]-1, split[0] );
-								<?php
-								if ( isset( $booking_settings['enable_inline_calendar'] ) && $booking_settings['enable_inline_calendar'] == 'on' ) { 
-								?>
-									var timestamp = Date.parse( bookingDate ); 
-									if ( isNaN( timestamp ) == false ) { 
-										var default_date_selection = new Date( timestamp );
-										jQuery( "#inline_calendar_checkout" ).datepicker( "setDate", default_date_selection );
-										jQuery( "#inline_calendar_checkout" ).datepicker( "option", "minDate", default_date_selection );
-									}
-								<?php 
-								} else {
-								?>
-									jQuery( "#booking_calender_checkout" ).datepicker( "setDate", bookingDate );
-								<?php 
-								}?>
-								jQuery( ".single_add_to_cart_button" ).show();
-								jQuery( ".quantity" ).show();
-								bkap_calculate_price();
+						<script type="text/javascript">
+						function bkap_init() {
+							jQuery( "#wapbk_hidden_date" ).val( '<?php echo $hidden_date; ?>' );
+							var split = jQuery( "#wapbk_hidden_date" ).val().split( "-" );
+							var bookingDate = new Date( split[2], split[1]-1, split[0] );
+							<?php
+							if ( isset( $booking_settings['enable_inline_calendar'] ) && $booking_settings['enable_inline_calendar'] == 'on' ) { 
+							?>
+								var timestamp = Date.parse( bookingDate ); 
+								if ( isNaN( timestamp ) == false ) { 
+									var default_date_selection = new Date( timestamp );
+									jQuery( "#inline_calendar" ).datepicker( "setDate", default_date_selection );
+								}
+							<?php 
 							} else {
-								jQuery( ".single_add_to_cart_button" ).show();
-								jQuery( ".quantity" ).show();
-								// This is called to ensure the special pricing for days/dates is displayed
-								bkap_single_day_price();
-							}
-							// Availability Display for the date selected only if setting is enabled
-							if ( jQuery( "#wapbk_availability_display" ).val() == "yes" ) {
+							?>
+								jQuery( "#booking_calender" ).datepicker( "setDate", bookingDate );
+							<?php 
+							}?>
+							if ( jQuery( "#wapbk_bookingEnableTime" ).val() == "on" && jQuery( "#wapbk_booking_times" ).val() == "YES" ) {
+								var sold_individually = jQuery( "#wapbk_sold_individually" ).val();
+								jQuery( "#ajax_img" ).show();
+								jQuery( ".single_add_to_cart_button" ).hide();	
+								var time_slots_arr = jQuery( "#wapbk_booking_times" ).val();
 								var data = {
-										checkin_date: jQuery( "#wapbk_hidden_date" ).val(),
-										post_id: '<?php echo $duplicate_of; ?>',
-										action: 'bkap_get_date_lockout'
-										};
-				
-								jQuery.post( '<?php echo get_admin_url() . 'admin-ajax.php'; ?>', data, function(response)
-								{
-									jQuery( "#show_stock_status" ).html(response);
+									current_date: jQuery( "#wapbk_hidden_date" ).val(),
+									post_id: '<?php echo $duplicate_of; ?>', 
+									action: 'bkap_check_for_time_slot'
+									<?php echo $attribute_fields_str; ?>
+								};
+								jQuery.post( '<?php echo get_admin_url() . 'admin-ajax.php'; ?>', data, function( response ) {
+									jQuery( "#ajax_img" ).hide();
+									jQuery( "#show_time_slot" ).html( response );
+									// for today if the time slot is past, display a message saying the same
+									<?php 
+									if ( isset( $past_time ) && $past_time == 0 ) {
+									?>
+										// Pre-populate the time slots with the order time slots
+										jQuery( "#time_slot" ).val( '' );
+									<?php 
+									} else {?>
+										// Pre-populate the time slots with the order time slots
+										jQuery( "#time_slot" ).val( '<?php echo $booking_time; ?>' );
+										var time_slot_value = jQuery( "#time_slot" ).val();
+										// Availability display for the time slot selected if setting is enabled			
+										if ( typeof time_slot_value != "undefined" && jQuery( "#wapbk_availability_display" ).val() == "yes" ) {
+											var data = {
+												checkin_date: jQuery( "#wapbk_hidden_date" ).val(),
+												timeslot_value: time_slot_value,
+												post_id: '<?php echo $duplicate_of; ?>', 
+												action: 'bkap_get_time_lockout'
+											};
+											jQuery.post( '<?php echo get_admin_url() . 'admin-ajax.php'; ?>', data, function( response ) {
+												jQuery( "#show_stock_status" ).html( response );
+											});
+										}
+											
+										if ( jQuery( "#time_slot" ).val() != "" ) {
+		                                    jQuery( ".payment_type" ).show();
+											if( sold_individually == "yes" ) {
+												jQuery( ".quantity" ).hide();
+												jQuery( ".payment_type" ).hide();
+												jQuery(".partial_message").hide();
+											} else {
+												jQuery( ".quantity" ).show();
+												jQuery( ".payment_type" ).show();
+											}
+										} else if ( jQuery("#time_slot").val() == "" ) {
+											jQuery( ".single_add_to_cart_button" ).hide();
+											jQuery( ".quantity" ).hide();
+		                                    jQuery( ".payment_type" ).hide();
+											jQuery( ".partial_message" ).hide();
+										}
+										// This is called to ensure the variable pricing for time slots is displayed
+										bkap_single_day_price();
+									<?php 
+									}?>
 								});
+							} else {
+								// check if multiple day is enabled, if yes then pre-populate the end date
+								if ( jQuery( "#booking_calender_checkout" ).length ) {
+									jQuery( "#wapbk_hidden_date_checkout" ).val( '<?php echo $hidden_date_checkout; ?>' );
+									var split = jQuery( "#wapbk_hidden_date_checkout" ).val().split( "-" );
+									var bookingDate = new Date( split[2], split[1]-1, split[0] );
+									<?php
+									if ( isset( $booking_settings['enable_inline_calendar'] ) && $booking_settings['enable_inline_calendar'] == 'on' ) { 
+									?>
+										var timestamp = Date.parse( bookingDate ); 
+										if ( isNaN( timestamp ) == false ) { 
+											var default_date_selection = new Date( timestamp );
+											jQuery( "#inline_calendar_checkout" ).datepicker( "setDate", default_date_selection );
+											jQuery( "#inline_calendar_checkout" ).datepicker( "option", "minDate", default_date_selection );
+										}
+									<?php 
+									} else {
+									?>
+										jQuery( "#booking_calender_checkout" ).datepicker( "setDate", bookingDate );
+									<?php 
+									}?>
+									jQuery( ".single_add_to_cart_button" ).show();
+									jQuery( ".quantity" ).show();
+									bkap_calculate_price();
+								} else {
+									jQuery( ".single_add_to_cart_button" ).show();
+									jQuery( ".quantity" ).show();
+									// This is called to ensure the special pricing for days/dates is displayed
+									bkap_single_day_price();
+								}
+								// Availability Display for the date selected only if setting is enabled
+								if ( jQuery( "#wapbk_availability_display" ).val() == "yes" ) {
+									var data = {
+											checkin_date: jQuery( "#wapbk_hidden_date" ).val(),
+											post_id: '<?php echo $duplicate_of; ?>',
+											action: 'bkap_get_date_lockout'
+											};
+					
+									jQuery.post( '<?php echo get_admin_url() . 'admin-ajax.php'; ?>', data, function(response)
+									{
+										jQuery( "#show_stock_status" ).html(response);
+									});
+								}
 							}
 						}
-						
-					}
-					window.onload = bkap_init;
-					</script>
-					<?php 
-				} else {
-					if ( isset( $past_date ) && $past_date == 0 ) {
-					?>
-						<script type="text/javascript">
-						function bkap_show_status() {
-							jQuery( "#show_stock_status" ).html( "The date <?php echo $display_date; ?> is past today. Please select another date" );
-						}
-						window.onload = bkap_show_status;
+						window.onload = bkap_init;
 						</script>
-					<?php 
-					}
-					else {
-					?>
-						<script type="text/javascript">
-						function bkap_show_status() {
-							jQuery( "#show_stock_status" ).html( "There are no longer any spaces available for <?php echo $display_date; ?>" );
+						<?php 
+					} else {
+						if ( isset( $past_date ) && $past_date == 0 ) {
+						?>
+							<script type="text/javascript">
+							function bkap_show_status() {
+								jQuery( "#show_stock_status" ).html( "The date <?php echo $display_date; ?> is past today. Please select another date" );
+							}
+							window.onload = bkap_show_status;
+							</script>
+						<?php 
 						}
-						window.onload = bkap_show_status;
-						</script>
-				<?php
+						else {
+						?>
+							<script type="text/javascript">
+							function bkap_show_status() {
+								jQuery( "#show_stock_status" ).html( "There are no longer any spaces available for <?php echo $display_date; ?>" );
+							}
+							window.onload = bkap_show_status;
+							</script>
+						<?php
+						}
 					}
 				}
 			}
